887e0abe1121df70cb416ca965a74285a2aef74d
Chih-Hsu <chihweih27@gmail.com>
2023-01-20 17:20:37 +0800

    Navigate to list fragment

--- app/src/main/java/com/chihwhsu/noodoeassigment/login/LoginFragment.kt
+++ app/src/main/java/com/chihwhsu/noodoeassigment/login/LoginFragment.kt
@@ -7,22 +7,21 @@ import android.view.ViewGroup
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import com.chihwhsu.noodoeassigment.data.UserInformationBody
+import androidx.navigation.fragment.findNavController
+import com.chihwhsu.noodoeassigment.R
 import com.chihwhsu.noodoeassigment.databinding.FragmentLoginBinding
 import com.chihwhsu.noodoeassigment.ext.getViewModelFactory
 
 class LoginFragment : Fragment() {
 
-    private val viewModel by viewModels<LoginViewModel>{ getViewModelFactory() }
+    private val viewModel by viewModels<LoginViewModel> { getViewModelFactory() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentLoginBinding.inflate(inflater,container,false)
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         binding.textInputEmail.doAfterTextChanged {
             viewModel.setUsername(it.toString())
@@ -36,10 +35,15 @@ class LoginFragment : Fragment() {
             viewModel.logIn()
         }
 
+        viewModel.navigation.observe(viewLifecycleOwner) {
+            if (it == true) {
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToParkingLotsFragment())
+            }
+        }
 
+        viewModel.error.observe(viewLifecycleOwner) {
 
-
-
+        }
 
         return binding.root
     }
--- app/src/main/java/com/chihwhsu/noodoeassigment/login/LoginViewModel.kt
+++ app/src/main/java/com/chihwhsu/noodoeassigment/login/LoginViewModel.kt
@@ -1,6 +1,8 @@
 package com.chihwhsu.noodoeassigment.login
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chihwhsu.noodoeassigment.data.Result
@@ -10,6 +12,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class LoginViewModel(private val repository: Repository) : ViewModel() {
+
+    private var _navigation = MutableLiveData<Boolean>().also { it.value = false }
+    val navigation : LiveData<Boolean> get() = _navigation
+
+    private var _error = MutableLiveData<String>()
+    val error : LiveData<String> get() = _error
+
 
 
     private var userName: String? = null
@@ -27,27 +36,30 @@ class LoginViewModel(private val repository: Repository) : ViewModel() {
     fun logIn() {
 
         if (isFormatCorrect(userName, password)) {
-
             val userInfo = UserInformationBody(userName!!, password!!)
+
             viewModelScope.launch(Dispatchers.Default) {
+
                 when(val result = repository.logIn(userInfo)){
 
                     is Result.Success -> {
-                        Log.d("testt", "$result")
+                        _navigation.postValue(true)
                     }
 
                     is Result.Error -> {
-                        Log.d("testt", "${result.exception}")
+                        _error.postValue(result.exception.message)
                     }
 
                     else ->{
 
                     }
-
                 }
-
             }
         }
+    }
+
+    fun doneNavigation(){
+        _navigation.value = false
     }
 
     private fun isFormatCorrect(userName: String?, password: String?): Boolean {
